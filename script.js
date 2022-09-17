a5aa5e39cf42307c37f8174f98eaf62b6b164e0f
reda0x <111064828+reda0x@users.noreply.github.com>
2022-09-17 07:41:48 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -7,3 +7,19 @@ hide.onclick = () => {
 show.onclick = () => {
     bar.style.right = '0'
 }
+
+let header = document.getElementById('header')
+
+window.addEventListener('scroll', function(){
+    let lastScroll = 0
+    const scrolled = window.pageYOffset;
+    if (scrolled >= 450){
+       header.classList.replace('header', 'header-scroll')
+
+    }else{
+        header.classList.replace('header-scroll', 'header')
+    }
+
+    lastScroll = scrolled
+    console.log(scrolled)
+})
